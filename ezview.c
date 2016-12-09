d1cc65c099e8d34e65058dacd5cb06978ba64f07
Matthew Robertson <mrr255@nau.edu>
2016-12-08 18:26:34 -0700

    reworked image importing, added ppmr.h

--- ezview.c
+++ ezview.c
@@ -2,7 +2,6 @@
 
 #define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2.h>
-//#include <OpenGL/gl.h>
 #include <GLFW/glfw3.h>
 
 #include "linmath.h"
@@ -21,12 +20,21 @@ typedef struct {
   float TexCoord[2];
 } Vertex;
 
+typedef struct {
+  float scale;
+  float rotate;
+  float translate[2]; // 0 -> x, 1 -> y
+  float shear[2];     // 0 -> x, 1 -> y
+} transvals
+
 // (-1, 1)  (1, 1)
 // (-1, -1) (1, -1)
 const GLubyte Indices[] = {
   0, 1, 2,
   2, 3, 0
 };
+
+
 
 Vertex vertexes[] = {
   {{1, -1}, {0.99999, 0}},
@@ -84,236 +92,26 @@ void glCompileShaderOrDie(GLuint shader) {
   }
 }
 
-// 4 x 4 image..
-unsigned char image[] = {
-  255, 0, 0, 255,
-  255, 0, 0, 255,
-  255, 0, 0, 255,
-  255, 0, 0, 255,
-
-  0, 255, 0, 255,
-  0, 255, 0, 255,
-  0, 255, 0, 255,
-  0, 255, 0, 255,
-
-  0, 0, 255, 255,
-  0, 0, 255, 255,
-  0, 0, 255, 255,
-  0, 0, 255, 255,
-
-  255, 0, 255, 255,
-  255, 0, 255, 255,
-  255, 0, 255, 255,
-  255, 0, 255, 255
-};
-
-// Helper method to parse from Ascii
-int getAscii(FILE *fr, char *temp)
-  {
-    int c = fgetc(fr);
-    int i = 0;
-    while(c != '\n' && c != ' ' && c != '\t') //Collect all characters before whitespace
-      {
-      temp[i]=c; //Add it to a buffer
-      i+=1;
-      c = fgetc(fr);
-      if(i>3)
-        {
-        fprintf(stderr, "%s\n", "Error: file not of type declared in format header");
-        exit(1);
-        }
-      }
-    int t = atoi(temp); //Convert to an integer
-/*
-    if(t > 255)
-      {
-      fprintf(stderr, "%s\n", "Error: multibyte color is not supported.");
-      return(1);
-      }
-      */
-    //printf("%i", t); //Debug Output
-    return(t);
-  }
-
-//Helper method to parse from Raw data
-int getByte(FILE *fr, char *temp)
-  {
-  int c = fgetc(fr); //Grab next character
-  /*
-  if(c == ' ' || c == '\n')
-  {
-  fprintf(stderr, "%s\n", "Error: file not of type declared in ppm header.");
-  exit(1);
-  }
-  */
-  int t = c; //Store as an int
-  /*
-  if(t > 255)
-    {
-    fprintf(stderr, "%s\n", "Error: multibyte color is not supported.");
-    return(1);
-    }
-    */
-  //printf("%i", t);//Debug Output
-  return(t);
-  }
-
-
 Pixel* parse(FILE* fr,int* w, int* h)
 {
-  //Parse Header
-  int c = fgetc(fr); // Get 'P'
-  int fileType = 0;
-  if (c != 'P')
-    {
-    fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
-    exit(1);
-    }
-  c = fgetc(fr); // Get 'X' -- Should be 3 or 6
-  if( c == '3')
-    {
-    fileType = 3;
-    //printf("Type 3\n"); //Debug Output
-    }
-  else if( c == '6')
-    {
-    fileType = 6;
-    //printf("Type 6\n"); //Debug Output
-    }
-  else
-    {
-    fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
-    exit(1);
-    }
 
-  c = fgetc(fr); //Get '\n'
-  //printf("%c\n", c); //Debug Output
-
-  if (c != '\n' && c!= ' ' && c!= '\t')
-    {
-    fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
-    exit(1);
-    }
-  c = fgetc(fr); //Get Next character -- should be comment, or width/height
-  int d; //comment character variable
-  while (c == '#') //Process the comment line(s)
-    {
-    d = fgetc(fr);
-    while (d!= '\n') //Write the rest of the comment to the output file
-      {
-      d = fgetc(fr);
-      }
-      c = fgetc(fr); //Get next char -- either # or width/height
-    }
-
-  //Parse Height/Width
-  //printf("%s\n", "Height"); //Debug Output
-  //printf("%c\n", c); //Debug Output
-
-  char *height; //Create array to hold characters of the height
-  height = malloc(16 * 5);
-  int i = 0;
-  while(c != ' ') //Get all characters before the first space
-    {
-    height[i] = c; //Add them to the array
-    c = fgetc(fr);
-    //printf("%c\n", c); //Debug Output
-    i+=1;
-    }
-  *h = atoi(height); //Convert character array to an Integer value
-
-
-  //printf("%s\n", "width"); //Debug Output
-
-  char *width; //Create array to hold characters of the width
-  width = malloc(16 * 5);
-  i = 0;
-  while(c != '\n') //Get all characters before the end of the line
-    {
-    width[i] = c; //Add them to the array
-    c = fgetc(fr);
-    //printf("%c\n", c); //Debug Output
-    i+=1;
-    }
-  *w = atoi(width); //Convert character array to an Integer value
-
-  //printf("%i\n",h); //Debug Output
-  //printf("%i\n",w); //Debug Output
-
-
-  //Parse Max Color Value
-  //printf("%s\n", "maxColor"); //Debug Output
-
-  char *maxColor; //Create array to hold characters of the max color
-  maxColor = malloc(16 * 5);
-  i = 0;
-  c = fgetc(fr);
-  while(c != '\n') //Get all characters before the end of the line
-    {
-    maxColor[i] = c; //Add them to the array
-    //printf("%c\n", c); //Debug Output
-    i+=1;
-    c = fgetc(fr);
-    }
-  int mC = atoi(maxColor); //Convert character array to an Integer value
-
-  if(mC > 255)
-    {
-    fprintf(stderr, "%s\n", "Error: multibyte color is not supported.");
-    exit(1);
-    }
-  //printf("%i\n",mC); //Debug Output
-
-
-  //Parsing of image data
-  Pixel* image = malloc(sizeof(Pixel) * *w * *h); //Prepare memory for image data
-  unsigned char temp[32]; //Create buffer for data
-
-  if (fileType == 3) //Collect Ascii Image data
-    {
-    int row, col;
-    for (row = 0; row < *h; row += 1)
-      {
-      for (col = 0; col < *w; col += 1) //Itterate through the image and add it to the structures
-        {
-        image[*w*row + col].r = getAscii(fr,temp);
-        //printf("%d ", image[w*row + col].r); //Debug Output
-        image[*w*row + col].g = getAscii(fr,temp);
-        //printf("%d ", image[w*row + col].g); //Debug Output
-        image[*w*row + col].b = getAscii(fr,temp);
-        //printf("%d  ", image[w*row + col].b); //Debug Output
-        }
-      //printf("\n"); //Debug Output
-      }
-    }
-  else //Collect Raw Image data
-    {
-    int row, col;
-    for (row = 0; row < *h; row += 1)
-      {
-      for (col = 0; col < *w; col += 1) //Itterate through the image and add it to the structures
-        {
-        image[*w*row + col].r = getByte(fr,temp);
-        //printf("%i ", image[w*row + col].r); //Debug Output
-        image[*w*row + col].g = getByte(fr,temp);
-        //printf("%i ", image[w*row + col].g); //Debug Output
-        image[*w*row + col].b = getByte(fr,temp);
-        //printf("%i  ", image[w*row + col].b); //Debug Output
-        }
-      //printf("\n"); //Debug Output
-      }
-
-  }
-  return image;
 }
 int main(int argc, char *argv[])
 {
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "ERROR: Usage ezview input.ppm\n", argc);
+    exit(1);
+  }
+
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program, index_buffer;
     GLint mvp_location, vpos_location, vcol_location;
+    Pixel *buffer;
+    transvals trans;
+    int        iw, ih, cMax, version;
 
-    int* iw= malloc(sizeof(int));
-    int* ih= malloc(sizeof(int));
     FILE* fr = fopen(argv[1], "r"); // File Read
     //Check if input file exists
     if(fr == NULL)
@@ -321,12 +119,28 @@ int main(int argc, char *argv[])
       fprintf(stderr, "%s\n", "Error: input file type not found.");
       return(1);
       }
-    Pixel* image = parse(fr,iw,ih);
+    Pixel* image = parseH(fr,&iw,&ih,&cMax,&version);
 
+    buffer = (pixel *) malloc(sizeof(pixel) * width * height);
+
+    if (version == 3)
+    {
+      readP3(fr, buffer, &iw, &ih, &cMax);
+    }
+    else
+    {
+      readP6(fr, buffer, &iw, &ih, &cMax);
+    }
+    fclose(fr);
+
+
+    //GLFW SETUP
     glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
+    if (!glfwInit()) {
+      fprintf(stderr, "Could not initialize GLFW.\n");
+      exit(1);
+    }
 
     glfwDefaultWindowHints();
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
@@ -334,25 +148,23 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "EZVIEW", NULL, NULL);
     if (!window)
     {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "Window init faild.\n");
+      glfwTerminate();
+      exit(1);
     }
 
     glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
-    // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     glfwSwapInterval(1);
-
-    // NOTE: OpenGL error checks have been omitted for brevity
 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
-
+    ////
     glGenBuffers(1, &index_buffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
@@ -369,7 +181,6 @@ int main(int argc, char *argv[])
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
-    // more error checking! glLinkProgramOrDie!
 
     mvp_location = glGetUniformLocation(program, "MVP");
     assert(mvp_location != -1);
@@ -384,32 +195,19 @@ int main(int argc, char *argv[])
     assert(tex_location != -1);
 
     glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location,
-			  2,
-			  GL_FLOAT,
-			  GL_FALSE,
-                          sizeof(Vertex),
-			  (void*) 0);
+    glVertexAttribPointer(vpos_location,2,GL_FLOAT,GL_FALSE,sizeof(Vertex),(void*) 0);
 
     glEnableVertexAttribArray(texcoord_location);
-    glVertexAttribPointer(texcoord_location,
-			  2,
-			  GL_FLOAT,
-			  GL_FALSE,
-                          sizeof(Vertex),
-			  (void*) (sizeof(float) * 2));
-
-    int image_width = *iw;
-    int image_height = *ih;
+    glVertexAttribPointer(texcoord_location,2,GL_FLOAT,GL_FALSE,sizeof(Vertex),(void*) (sizeof(float) * 2));
 
     GLuint texID;
     glGenTextures(1, &texID);
     glBindTexture(GL_TEXTURE_2D, texID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB,
-		 GL_UNSIGNED_BYTE, image);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, iw, ih, 0, GL_RGB,
+		 GL_UNSIGNED_BYTE, buffer);
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texID);
---
+++ ppmr.h
@@ -0,0 +1,110 @@
+#ifndef PPMR
+#define PPMR
+
+#ifndef STD_LIB
+#define STD_LIB
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <string.h>
+#include <ctype.h>
+#endif
+
+
+typedef struct Pixel {
+  unsigned char r, g, b;
+} Pixel;
+
+/* Function Prototypes */
+static inline  void   readP3(FILE *in, Pixel *buffer, int *width,
+                               int *height, int *maxColor);
+static inline  void   readP6(FILE *in, Pixel *buffer, int *width,
+                               int *height, int *maxColor);
+static inline  int    parseH(FILE *fr, int *width, int *height,
+                               int *maxColor, int *version);
+
+//parse the header
+static inline int parseH(FILE *fr, int *width, int *height,
+                           int *maxColor, int *version)
+{
+  char c; // Character value
+
+  fscanf(fr, "P%c\n", &c);
+  if (c != '3' && c != '6') //valid type?
+  {
+      return 1;
+  }
+  *version = atoi(&c); //store version
+
+  c = getc(fr);   // handle comments
+  while (c == '#')
+  {
+    do
+    {
+      c = getc(fr);
+    }
+    while (c != '\n');
+    c = getc(fr);
+  }
+
+  //should be a number next
+  if (!isdigit(c))
+  {
+    return 1;
+  }
+  ungetc(c, fr);
+
+
+  //grab values from header
+  fscanf(fr, "%d%d%d\n", width, height, maxColor);
+
+  return 0;
+}
+
+
+static inline void readP3(FILE *in, Pixel *buffer, int *width, int *height, int *maxColor)
+  {
+  int c;
+  int r, g, b;
+  int i = 0;
+  int arryMax = *width * *height;
+
+
+  while (i < arryMax) {
+      if ((c = fgetc(in)) != EOF) //if still values left
+      {
+        ungetc(c, in);
+
+        //get pixel
+        if (fscanf(in, "%d%d%d", &r, &g, &b) == 3) {
+          buffer[i].r = (unsigned char) r;
+          buffer[i].g = (unsigned char) g;
+          buffer[i].b = (unsigned char) b;
+        }
+      i++;
+    }
+  }
+}
+
+static inline void readP6(FILE *in, Pixel *buffer, int *width,
+                            int *height, int *maxColor) {
+  unsigned char *charBuffer = malloc(sizeof(unsigned char));
+  int i = 0;
+  int color;
+  int arryMax = *width * *height;
+
+  while (fread(charBuffer, 1, 1, in) && i < arryMax) //add pixels to rgb
+  {
+    color = *charBuffer;
+    buffer[i].r = (unsigned char) color;
+    fread(charBuffer, 1, 1, in);
+    color = *charBuffer;
+    buffer[i].g = (unsigned char) color;
+    fread(charBuffer, 1, 1, in);
+    color = *charBuffer;
+    buffer[i].b = (unsigned char) color;
+    i++;
+  }
+}
+
+#endif
